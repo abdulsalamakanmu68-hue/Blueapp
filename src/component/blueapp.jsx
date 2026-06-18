6ea8b9bcfce01a7075c8c10d5ebdcb39b1a2dd17
abdulsalamakanmu68-hue <abdulsalamakanmu68@gmail.com>
2026-06-18 22:08:56 +0100

    blueapp.jsx

--- src/component/blueapp.jsx
+++ src/component/blueapp.jsx
@@ -7,26 +7,7 @@ const BellAliantLogin = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-      try {
-    const response = await fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-  
-      }),
-    });
-
-    const data = await response.json();
-
-    console.log("Sent successfully:", data);
-
-  } catch (error) {
-    console.error("Error sending data:", error);
-  }
+    
   };
 
   return (
